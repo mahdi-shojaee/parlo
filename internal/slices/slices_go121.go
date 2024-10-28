fee79dcb771ff902910b754103b4f43f7f0a51e2
Mahdi Shojaee <msh6180339@gmail.com>
2024-10-28 19:37:13 +0330

    fix: fix compatibility with Go 1.21

--- internal/slices/slices_go121.go
+++ internal/slices/slices_go121.go
@@ -42,9 +42,34 @@ func Clone[S ~[]E, E any](s S) S {
 	return append(s[:0:0], s...)
 }
 
+// Grow increases the slice's capacity, if necessary, to guarantee space for
+// another n elements. After Grow(n), at least n elements can be appended
+// to the slice without another allocation. If n is negative or too large to
+// allocate the memory, Grow panics.
+func Grow[S ~[]E, E any](s S, n int) S {
+	if n < 0 {
+		panic("cannot be negative")
+	}
+	if n -= cap(s) - len(s); n > 0 {
+		s = append(s[:cap(s)], make([]E, n)...)[:len(s)]
+	}
+	return s
+}
+
 // Concat returns a new slice concatenating the passed in slices.
 func Concat[S ~[]E, E any](slices ...S) S {
-	return sls.Concat(slices...)
+	size := 0
+	for _, s := range slices {
+		size += len(s)
+		if size < 0 {
+			panic("len out of range")
+		}
+	}
+	newslice := Grow[S](nil, size)
+	for _, s := range slices {
+		newslice = append(newslice, s...)
+	}
+	return newslice
 }
 
 // BinarySearch searches for target in a sorted slice and returns the earliest
